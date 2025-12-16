d7762b16a76ce0105e151e9740e048f5a31f014c
gitychzh <93921526@163.com>
2025-12-16 16:25:04 +0800

    新增了outDir 以及部署代码

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "rspress/config";
 export default defineConfig({
     // 文档根目录
     root: "docs",
+    outDir: "rspressPage", //新增的，与目标目录一致，方便写部署代码
     // base: "/Autox_ozobi_Docs", // 如果是生成 autox app 的本地文档，需要注释掉这一行
     title: "Autox_ozobi_Docs",
     markdown: {
